64ed05d12b3d60fb3c4a5e2dbf8a9bc5ba18aaba
Jorge Ramón <jorge.ramon2128@gmail.com>
2021-07-13 21:03:49 -0500

    Agregando solución del reto 1

--- sesion06/inventario_tienda.py
+++ sesion06/inventario_tienda.py
@@ -8,10 +8,38 @@ class Producto:
 
 inventario = []
 
+def agregar_producto():
+  print("Ingresa el nombre:")
+  nombre = input()
+  
+  print("Ingresa el precio:")
+  precio = input()
+
+  inventario.append(Producto(nombre, precio))
+  almacenar_producto(nombre, precio)
+  print("\nProducto agregado")
+
+def ver_productos():
+  print("Inventario:\n")
+  for producto in inventario:
+    print(producto)
+
 # 1. Cargar el inventario del archivo "inventario.txt"
+def cargar_productos():
+  with open("archivos/inventario.txt") as archivo:
+    lineas = archivo.read().splitlines()
+    for linea in lineas:
+      linea_split = linea.split("|")
+      inventario.append(Producto(linea_split[0], linea_split[1]))
+
 # 2. Almacenar el inventario en el mismo archivo
+def almacenar_producto(nombre, precio):
+  with open("archivos/inventario.txt", "a") as archivo:
+    archivo.write("\n{}|{}".format(nombre, precio))
 
 salir = False
+
+cargar_productos()
 
 while not salir:
   print("\n")
@@ -24,16 +52,9 @@ while not salir:
   print("\n")
 
   if opcion == "2":
-    for producto in inventario:
-      print(producto)
+    ver_productos()
   elif opcion == "1":
-    print("Ingresa el nombre:")
-    nombre = input()
-    print("Ingresa el precio:")
-    precio = input()
-
-    inventario.append(Producto(nombre, precio))
-    print("\nProducto agregado")
+    agregar_producto()
   elif opcion == "3":
     salir = True
   else:
